a69903c4b5c58fc7e231936f9675dedaafde7efc
AZAOWEN <azaowen22@gmail.com>
2024-05-27 18:17:48 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var fullName ='vit con be bong'
+setInterval = function(){
+//console.log('hihi'+ Math.random,1000)},1000)
+
+
+
+};
+
+
+
+
+
+// hihihihi
+let registration = document.querySelector('.registration');
+let showup2 = document.querySelector('.showup2');
+
+
+
+showup2.onclick = function(){
+    registration.classList.add('active');
+    showup2.classList.add('active');
+};
+
+// hihihihi
